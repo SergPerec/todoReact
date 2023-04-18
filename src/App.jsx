32b6086a337cd254f4a483f947851fa183430d84
Stro_Serg <Prahatuc@mail.ru>
2023-04-18 12:36:08 +0300

    add functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,52 @@
+import { React, useState, useEffect } from 'react';
+import TodoBlock from './components/TodoBlock';
+import './App.css';
+
 function App() {
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem('todos')) || []
+  );
+
+  const addTodo = (todo) => {
+    setTodos([...todos, todo]);
+    localStorage.setItem('todos', JSON.stringify([...todos, todo]));
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((item) => item.id !== id));
+  };
+
+  const changeStatus = (id, status) => {
+    setTodos(
+      todos.map((item) => (item.id === id ? { ...item, status } : item))
+    );
+  };
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
   return (
-    <>
-      <h1>hello world</h1>
-      <p>may name is Serg</p>
-    </>
+    <div className="container">
+      <div className="section">
+        <TodoBlock
+          addTodo={addTodo}
+          deleteTodo={deleteTodo}
+          todos={todos}
+          changeStatus={changeStatus}
+          title="HIGH"
+          priority="high"
+        />
+        <TodoBlock
+          addTodo={addTodo}
+          deleteTodo={deleteTodo}
+          todos={todos}
+          changeStatus={changeStatus}
+          title="LOV"
+          priority="lov"
+        />
+      </div>
+    </div>
   );
 }
 
